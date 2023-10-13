b9ededd96b555f10f95c0f995474f14e996e5efb
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2023-10-13 14:26:28 +0600

    image styled done

--- src/components/ui/heroSlider/HeroSlider.tsx
+++ src/components/ui/heroSlider/HeroSlider.tsx
@@ -1,9 +1,9 @@
 import { Carousel } from "antd";
 import Image from "next/image";
 import React from "react";
-import heroImage1 from "../../../assets/heroSlider1.jpg";
-import heroImage2 from "../../../assets/heroSlider2.jpeg";
-import heroImage3 from "../../../assets/heroSlider3.jpeg";
+import heroImage1 from "../../../assets/heroImage1.jpg";
+import heroImage2 from "../../../assets/heroImage2.jpg";
+import heroImage3 from "../../../assets/heroImage3.jpg";
 
 const contentStyle: React.CSSProperties = {
   height: "160px",
@@ -14,57 +14,16 @@ const contentStyle: React.CSSProperties = {
 };
 
 const HeroSlider: React.FC = () => (
-  <div className=" bg-black">
+  <div className=" ">
     <Carousel autoplay>
       <div className="w-full">
-        <Image
-          src={heroImage1}
-          height={500}
-          width={500}
-          //   fill
-          //   height={100}
-          //   sizes="(min-width: 808px) 50vw, 100vw"
-          //   style={{
-          //     objectFit: "cover",
-          //     height: "80%",
-          //     width: "100%",
-          //   }}
-          alt="hrro bannar 1"
-        />
+        <Image src={heroImage1} height={100} width={1246} alt="hrro bannar 1" />
       </div>
       <div className="w-full">
-        <Image
-          src={heroImage2}
-          height={500}
-          //   width={500}
-          //   fill
-          //   height={100}
-          //   sizes="(min-width: 808px) 50vw, 100vw"
-          style={
-            {
-              // objectFit: "cover",
-              //   height: "100%",
-              // width: "100%",
-            }
-          }
-          alt="hrro bannar 1"
-        />
+        <Image src={heroImage2} height={100} width={1246} alt="hrro bannar 1" />
       </div>
       <div className="w-full">
-        <Image
-          src={heroImage3}
-          height={500}
-          width={500}
-          //   fill
-          //   height={100}
-          //   sizes="(min-width: 808px) 50vw, 100vw"
-          //   style={{
-          //     objectFit: "cover",
-          //     height: "100%",
-          //     width: "100%",
-          //   }}
-          alt="hrro bannar 1"
-        />
+        <Image src={heroImage3} height={100} width={1246} alt="hrro bannar 1" />
       </div>
     </Carousel>
   </div>
