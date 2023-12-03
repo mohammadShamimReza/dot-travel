900a1539aa26d3841ed4928838b82ed5674aec1f
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-12-03 10:16:34 +0600

    chenge env

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -2,6 +2,7 @@ export const getBaseUrl = ():string => {
     
     return (
       process.env.NEXT_PUBLIC_API_BASE_URL ||
-      "https://dot-travel-backend.vercel.app/api/v1"
+      // "https://dot-travel-backend.vercel.app/api/v1"
+      "http://localhost:5000/api/v1"
     );
 }
