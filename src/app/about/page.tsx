83b9b77112186b4c3fb6ecfac75642e7f1c37304
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2024-05-01 21:54:35 +0600

    add skletion

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@
 import { useBlogQuery } from "@/redux/api/blogApi";
 import { useFAQQuery } from "@/redux/api/faqApi";
 import { IBlog, IFaq } from "@/types";
-import { Collapse, Divider, Typography } from "antd";
+import { Collapse, Divider, Skeleton, Typography } from "antd";
 
 const { Panel } = Collapse;
 const { Title, Text } = Typography;
@@ -13,23 +13,27 @@ const AboutUs = () => {
 
   return (
     <div className="max-w-5xl mx-auto p-4 md:p-8 min-h-screen">
-      {blogDatas?.data?.map((blogData: IBlog) => (
-        <div className="" key={blogData.id}>
-          <Title
-            level={2}
-            className="text-3xl md:text-4xl font-extrabold mb-4 md:mb-6 text-blue-500"
-          >
-            {blogData.title}
-          </Title>
+      {!blogDatas ? (
+        <Skeleton active />
+      ) : (
+        blogDatas?.data?.map((blogData: IBlog) => (
+          <div className="" key={blogData.id}>
+            <Title
+              level={2}
+              className="text-3xl md:text-4xl font-extrabold mb-4 md:mb-6 text-blue-500"
+            >
+              {blogData.title}
+            </Title>
 
-          <Text className="text-gray-700 mb-4 md:mb-6">
-            {blogData.description}
-          </Text>
-          <br />
-          <br />
-          <br />
-        </div>
-      ))}
+            <Text className="text-gray-700 mb-4 md:mb-6">
+              {blogData.description}
+            </Text>
+            <br />
+            <br />
+            <br />
+          </div>
+        ))
+      )}
 
       <Divider />
 
@@ -41,17 +45,21 @@ const AboutUs = () => {
       </Title>
 
       <Collapse defaultActiveKey={["1"]}>
-        {faqDatas?.data?.map((faqData: IFaq) => (
-          <Panel
-            header={faqData.title}
-            key={faqData.id}
-            className="bg-blue-50 text-center text-lg"
-          >
-            <Text className="text-gray-700 text-lg p-5">
-              {faqData.description}
-            </Text>
-          </Panel>
-        ))}
+        {!faqDatas ? (
+          <Skeleton active />
+        ) : (
+          faqDatas?.data?.map((faqData: IFaq) => (
+            <Panel
+              header={faqData.title}
+              key={faqData.id}
+              className="bg-blue-50 text-center text-lg"
+            >
+              <Text className="text-gray-700 text-lg p-5">
+                {faqData.description}
+              </Text>
+            </Panel>
+          ))
+        )}
       </Collapse>
     </div>
   );
