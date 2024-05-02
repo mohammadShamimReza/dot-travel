792231bb8b1299e1c914945ca16a8f8d9de5f1c2
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2024-05-03 03:10:43 +0600

    add things 5

--- src/app/TourPackages/page.tsx
+++ src/app/TourPackages/page.tsx
@@ -46,7 +46,7 @@ function TourPage() {
   if (tourPackages.data.meta.total === 0) {
     return (
       <div className="min-h-screen">
-        <h1 className="text-center text-9xl text-blue-600">Data not found</h1>
+        <h1 className="text-center text-3xl text-blue-600">Data not found</h1>
       </div>
     );
   }
